bfa8966fcb01d4f1b03b5899a1719257f4d5ec6d
wb_xiaohai.jin <jxh01778185@alibaba-inc.com>
2023-11-16 15:59:13 +0800

    xx

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import Admin from "../view/admin";
+import ShoppingCart from "../view/shoppingCart";
+const Router = () => {
+    const routerArr = [
+        {
+            path: '/admin',
+            name: 'Admin',
+            component: Admin
+        },
+        {
+            path: '/shoppingCart',
+            name: 'ShoppingCart',
+            component: ShoppingCart
+        },
+    ]
+    return (
+        <Routes>
+            {
+                routerArr.map((item, index) => {
+                    return (
+                        <Route path={item.path} element={<item.component />} key={index} />
+                    )
+                })
+            }
+        </Routes>
+    )
+}
+export default Router;
---
+++ src/view/shoppingCart/index.jsx
@@ -0,0 +1,8 @@
+const ShoppingCart = () => {
+    return (
+        <div>
+            <h1>Shopping Cart</h1>
+        </div>
+    )
+}
+export default ShoppingCart
